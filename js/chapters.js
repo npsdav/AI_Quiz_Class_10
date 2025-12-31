53bfd7e32a407928551a305909b11185915dd9ef
Nirender Prakash Singh <89131907+npsdav@users.noreply.github.com>
2025-12-31 20:15:18 +0530

    Add CHAPTER_CSV export for chapter data files

---
+++ js/chapters.js
@@ -0,0 +1,8 @@
+// js/chapters.js
+export const CHAPTER_CSV = {
+  chapter1_ai_project_cycle: 'data/chapters/chapter1_ai_project_cycle.csv',
+  chapter2_data_acquisition: 'data/chapters/chapter2_data_acquisition.csv',
+  chapter3_data_exploration: 'data/chapters/chapter3_data_exploration.csv',
+  chapter4_modelling: 'data/chapters/chapter4_modelling.csv',
+  chapter5_evaluation: 'data/chapters/chapter5_evaluation.csv',
+};
